bc4bcb95fb3fc7ef5238d60e603cdeed4f12a9ae
tossi <tosifraza8867@gmail.com>
2024-11-15 19:15:05 +0530

    some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -142,7 +142,7 @@ function App() {
 
   return (
     <div className="App">
-      {/* <SpeechRecognitionComponent /> */}
+      <SpeechRecognitionComponent />
       {seeLogs ? (
         <HistoryScreen setSeeLogs={setSeeLogs} />
       ) : status === 'start' ? (
