f49d9c904179b85bd12b59c50f26778750c22a27
root <you@example.com>
2024-11-23 20:25:43 +0530

    audio  media stream func corrected further for agent recording

--- src/hooks/useJssip.js
+++ src/hooks/useJssip.js
@@ -493,6 +493,28 @@ const useJssip = () => {
             });
           } else {
             console.log('e.session.direction is ', e.session.direction);
+            navigator.mediaDevices
+              .getUserMedia({ audio: true })
+              .then((stream) => {
+                // localStream = stream; // Store the local stream in a variable
+                // localvideo.srcObject = stream;
+                const socket = agentSocketRef?.current;
+                const { agentmediaRecorder, agentwebsocket, stop } = startspeechToText(stream, "Agent", socket);
+                //console.log(stop);
+                //console.log(stream);
+
+                stream.oninactive = function () {
+                  console.log('Stream ended');
+                  stop();
+                  //stopSpeechTotext(agentmediaRecorder, agentwebsocket);                
+                  // agentText = "";
+                  // customerText = "";
+                };
+
+              })
+              .catch((err) => {
+                console.log('error while getting media', err);
+              });
             e.session.answer();
             let localStream;
             // navigator.mediaDevices
